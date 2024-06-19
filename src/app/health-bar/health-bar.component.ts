7d2fadf12e027a182ba138df0b913431a1265292
romain <romdz01@gmail.com>
2024-06-19 17:24:10 +0200

    feat(health-bar) Input for progress value

--- src/app/health-bar/health-bar.component.ts
+++ src/app/health-bar/health-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MonsterHealthService } from '../services/monster-health.service';
 
 @Component({
   selector: 'app-health-bar',
@@ -8,5 +9,6 @@ import { Component } from '@angular/core';
   styleUrl: './health-bar.component.css'
 })
 export class HealthBarComponent {
-
+  constructor(public monsterHealthService: MonsterHealthService) { }
+  @Input() value: number = 100
 }
