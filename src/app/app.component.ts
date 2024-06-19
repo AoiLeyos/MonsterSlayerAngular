0798cad0a9e16969577033f4d964cd5f26e0f070
romain <romdz01@gmail.com>
2024-06-19 17:25:55 +0200

    feat(app.component) add import from service & component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HealthBarComponent } from './health-bar/health-bar.component';
+import { HealthComponent } from './health/health.component';
+import { HeroHealthService } from './services/hero-health.service';
+import { MonsterHealthService } from './services/monster-health.service';
+import { ButtonsComponent } from './buttons/buttons.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HealthBarComponent],
+  imports: [RouterOutlet,
+    HealthBarComponent,
+    HealthComponent,
+    ButtonsComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  constructor(public heroHealthService: HeroHealthService, public monsterHealthService: MonsterHealthService) { }
   title = 'MonsterSlayer';
 }
