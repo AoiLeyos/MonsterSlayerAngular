6b8078e59b38aef362a37482a6df480ac8dd27cb
romain <romdz01@gmail.com>
2024-06-19 09:33:46 +0200

    feat(app.component.ts)import health-bar.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HealthBarComponent } from './health-bar/health-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HealthBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
