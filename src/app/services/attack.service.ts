dfc034d36a6c2e72e3a25bdecfa7f5bfce5d65ac
romain <romdz01@gmail.com>
2024-06-20 13:29:21 +0200

    feat(attack.services.ts)add logs & reset Game

--- src/app/services/attack.service.ts
+++ src/app/services/attack.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HeroHealthService } from './hero-health.service';
 import { MonsterHealthService } from './monster-health.service';
 import { LogsService } from './logs.service';
+import { ResetGameService } from './reset-game.service';
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +11,32 @@ import { LogsService } from './logs.service';
 export class AttackService {
   constructor(public heroHealthService: HeroHealthService,
     public monsterHealthService: MonsterHealthService,
-    public logsService: LogsService) { }
+    public logsService: LogsService,
+    public resetGameService: ResetGameService) { }
 
-  heroAttack() {
+  Attack() {
     let monsterDamage = Math.floor(Math.random() * 21);
     let heroDamage = Math.floor(Math.random() * 21);
+
     this.monsterHealthService.currentHealth -= heroDamage
+    this.logsService.log(`Player hit Monster for ${heroDamage} damage, Monster health : ${this.monsterHealthService.currentHealth} .`)
     this.heroHealthService.currentHealth -= monsterDamage
-    this.logsService.log(`Player hit monster for ${heroDamage} damage, monster health ${this.monsterHealthService.currentHealth} .`)
+    this.logsService.log(`Monster hit Player for ${monsterDamage} damage Player health : ${this.heroHealthService.currentHealth} `)
+
+
+    if (this.heroHealthService.currentHealth === 0 && this.monsterHealthService.currentHealth === 0) {
+      alert("Equality");
+      this.resetGameService.resetGame()
+    } else if (this.heroHealthService.currentHealth === 0) {
+      alert("Monster win's");
+      this.resetGameService.resetGame();
+    }
+    else if (this.monsterHealthService.currentHealth === 0) {
+      alert("Player win");
+      this.resetGameService.resetGame()
+    }
   }
+
 
 
 }
