514651f69fbe96629d62f994daade64d323cd3a3
romain <romdz01@gmail.com>
2024-06-21 11:56:56 +0200

    feat(health-bar.componant.ts)import HerohealthService,HealthComponent & add constructor & Input for value

--- src/app/health-bar/health-bar.component.ts
+++ src/app/health-bar/health-bar.component.ts
@@ -1,14 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { MonsterHealthService } from '../services/monster-health.service';
+import { HeroHealthService } from '../services/hero-health.service';
+import { HealthComponent } from '../health/health.component';
 
 @Component({
   selector: 'app-health-bar',
   standalone: true,
-  imports: [],
+  imports: [HealthComponent],
   templateUrl: './health-bar.component.html',
   styleUrl: './health-bar.component.css'
 })
 export class HealthBarComponent {
-  constructor(public monsterHealthService: MonsterHealthService) { }
+  constructor(public monsterHealthService: MonsterHealthService,
+    public heroHealthService: HeroHealthService,
+  ) { }
   @Input() value: number = 100
 }
